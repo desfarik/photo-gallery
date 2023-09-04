5830f02d78beb47b70bed79499aafcc42c992c61
Dima Astreyko <d.astreyko@itransition.com>
2023-09-04 19:09:30 +0300

    add storage urls

--- src/app/photo-url.constants.ts
+++ src/app/photo-url.constants.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = ''
-export const MEDIUM_PHOTO_URL = (id: number) => `assets/photos-webp-medium/${id}.webp`
-export const BLUR_PHOTO_URL = (id: number) => `assets/photos-webp-blur/${id}.webp`
+export const BASE_URL = 'https://storage.googleapis.com/dima-and-svetlana-wedding.appspot.com'
+export const MEDIUM_PHOTO_URL = (id: number) => `${BASE_URL}/photos-webp-medium/${id}.webp`
+export const BLUR_PHOTO_URL = (id: number) => `${BASE_URL}/photos-webp-blur/${id}.webp`
